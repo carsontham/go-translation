bc09a71d6b232bf100ade6ac76c9c3fb58e36876
carsontham <carsontham@outlook.com>
2024-04-05 17:39:25 +0800

    allow CLI input

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -8,10 +8,16 @@ import (
 
 func main() {
 	url := "https://deep-translate1.p.rapidapi.com/language/translate/v2"
+	var input string
+	var target string
 
 	c := service.NewClient(url)
+	fmt.Print("Please enter the input: ")
+	fmt.Scanln(&input)
+	fmt.Print("Please enter the language to translate to: ")
+	fmt.Scanln(&target)
 
-	translated, err := c.Translate(context.Background(), "hello", "ja")
+	translated, err := c.Translate(context.Background(), input, target)
 	if err != nil {
 		fmt.Println(err)
 		return
---
+++ app/infrastructure/service/client_test.go
@@ -0,0 +1,19 @@
+package service_test
+
+import (
+	"go-translation/app/infrastructure/service"
+	"net/http"
+	"testing"
+)
+
+type translationClientConfig struct {
+	translateEndpoint string
+}
+
+func setUpTranslationClient(
+	t *testing.T,
+	config translationClientConfig,
+	stubResponses map[string]*http.Response,
+) (apiClient *service.Client) {
+	return
+}
